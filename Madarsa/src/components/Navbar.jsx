14c0b0ceb3b12796f1a0021555ccebec2f5a243d
Talha Tanvir <talhatanveer202@gmail.com>
2025-07-05 21:13:51 +0500

    Add New Things in Madarsa Website

--- Madarsa/src/components/Navbar.jsx
+++ Madarsa/src/components/Navbar.jsx
@@ -1,29 +1,28 @@
 import React from 'react'
 
-
 function Navbar() {
-
-  
   return (
-    <nav className='bg-[#0F766E] h-20 px-8 flex justify-between items-center text-white'>
-        
-            
-        <div className='text-xl'>
-          <p>شوال 1446ھ 12 اپریل 2025ء</p>
-        </div>
+    <nav dir='rtl' className='flex bg-[#0F766E] h-22 text-white items-center justify-between px-8'>
 
-        <ul className='flex gap-14 text-2xl cursor-pointer'>
-                <li>آن لائین داخلہ</li>
-                <li>دعائیں</li>
-                <li>کتابیں</li>
-                <li>بینات</li>
-                <li>دارالافتاء</li>
-                <li>سرورق</li>
-            </ul>
+      <div>
+        <img className='w-46' src="/images/logo.png" alt="" />
+      </div>
 
-        <div className='w-48'>
-            <img src="./public/images/logo.png" alt="" />
-        </div>
+      <div>
+        <ul className='flex text-xl gap-12'>
+          <li><a href="#">سرورق</a></li>
+          <li><a href="#">دارالافتاء</a></li>
+          <li><a href="#">بینات</a></li>
+          <li><a href="#">کتابیں</a></li>
+          <li><a href="#">دعائیں</a></li>
+          <li><a href="#">آن لائین داخلہ</a></li>
+        </ul>
+      </div>
+
+      <div>
+        <p dir='rtl' className='text-lg'>13 شوال 1446ھ &nbsp;&nbsp;&nbsp;&nbsp; 12 اپریل 2025 ء</p>
+      </div>
+      
     </nav>
   )
 }
